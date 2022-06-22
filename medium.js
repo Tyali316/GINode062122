cefbafef3ed230a589aafaea17dc3d5428f556d7
Tyali Warren <tyaliwarren@MacBook-Pro-3.lan>
2022-06-22 19:52:17 -0400

    done

---
+++ medium.js
@@ -0,0 +1,8 @@
+var http = require('http');
+function onRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write('Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune,Pluto');
+    response.end();
+}
+http.createServer(onRequest).listen(8000);
+
